483ae0d8e4d6c409f18a1277c2f84d39896cfa55
Stewie02 <stefansijbesma12@gmail.com>
2021-05-26 12:27:13 +0200

    Added the PiCamera

--- parts/vision/RaspberryCamera.py
+++ parts/vision/RaspberryCamera.py
@@ -1,4 +1,17 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+
+
 def read_frame():
     """Returns an image frame from the Raspberry PI camera."""
+    camera.capture(rawCapture, format="bgr")
+    return rawCapture.array
+
+
+def read_frame():
     # TODO: Return image that opencv can read
     pass
