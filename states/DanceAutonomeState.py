da68bdba2685948d4801dc3156e6981a0fadbf18
ElvedinIbric <55244146+ElvedinIbric@users.noreply.github.com>
2021-06-07 12:40:52 +0200

    opzet autonome dans

--- states/DanceAutonomeState.py
+++ states/DanceAutonomeState.py
@@ -5,19 +5,20 @@ from states.State import State
 from states.moves.ArmMove import ArmMove
 from states.moves.DriveMove import DriveMove
 from states.moves.GripMove import GripMove
-
-
-class _Navigator:
-    """A helper class that determines the pre-programmed routes to take because webots has no remote controller."""
-
-    def __init__(self):
-        self.route_index = 0
+#   import RPi.GPIO as GPIO
 
 
 class DanceAutonomeState(State):
     def __init__(self):
         RemoteControl.add_listener(self)
-        self._navigator = _Navigator()
+        # GPIO.setwarnings(False)  # Ignore warning for now
+        # GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
+        # GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
+        # GPIO.setup(9, GPIO.OUT, initial=GPIO.LOW)
+        # GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW)
+
+        self.route_index = 0
+        self.song_time = 120
 
         self._SPEEDS = [
             [7.0, 7.0],  # Forward
@@ -26,12 +27,27 @@ class DanceAutonomeState(State):
             [4.0, -4.0]  # Right
         ]
 
-        self.current_move = None
-        self.move_choice = None
-        self.time_for_move = (bpm / 60) * 2
         pass
 
     def step(self):
+        # TODO: code for VU sound action
+        # TODO: VU_Meter class
+        # if VU_value == HIGH
+        #     GPIO.output(8, GPIO.HIGH)  # Turn on
+        #     DriveMove(self._SPEEDS[2]) # Dance to the left
+        #     print("Spin to the left")
+        #     GPIO.output(8, GPIO.LOW)   # Turn off
+        # elif VU_value == MIDDLE
+        #     GPIO.output(9, GPIO.HIGH)  # Turn on
+        #     DriveMove(self._SPEEDS[3]) # Dance to the right
+        #     print("Spin to the right")
+        #     GPIO.output(9, GPIO.LOW)   # Turn off
+        # elif VU_value == LOW
+        #     GPIO.output(10, GPIO.HIGH)  # Turn on
+        #     ArmMove(self.time_for_move)
+        #     print("Move the arm")
+        #     GPIO.output(10, GPIO.LOW)   # Turn off
+
         pass
 
     def deactivate(self):
@@ -49,16 +65,3 @@ class DanceAutonomeState(State):
         # Don't handle mode switches
         if ControllerButton.is_mode_button(button):
             return
-
-    # def on_sound_change(self, VU_value):
-    #     """VU listener function"""
-    #     # TODO: code for VU sound action
-    #     if VU_value == HIGH
-    #         DriveMove(self._SPEEDS[2])
-    #         print("Spin to the left")
-    #     elif VU_value == MIDDLE
-    #         DriveMove(self._SPEEDS[3])
-    #         print("Spin to the right")
-    #     elif VU_value == LOW
-    #         ArmMove(self.time_for_move)
-    #         print("Move the arm")
