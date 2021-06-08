b29371cab019427653f130a6f6af1b9031167253
Stewie02 <stefansijbesma12@gmail.com>
2021-06-08 11:22:47 +0200

    The MicroController class is filled with the right functions

--- parts/MicroController.py
+++ parts/MicroController.py
@@ -1,3 +1,6 @@
+from smbus2 import SMBus as smbus
+import json
+
 
 
 class MicroController:
@@ -8,20 +11,20 @@ class MicroController:
         """An instance will be created if none exists
         Else an exception is raised.
         The I2C connection with the MicroController is also established"""
-        # TODO: needs to be implemented
-        pass
+        self.bus = smbus(1)
+        self.address = 0x08
 
     def show_on_display(self, text):
         """The microcontroller get a message to show the text in the parameters on the display
         text: String representing the text which must be shown on the display"""
-        # TODO: needs to be implemented
-        pass
+        self.bus.write_i2c_block_data(self.address, 0, json.dumps({ "display_text": text }))
 
     def get_weight(self):
         """Returns the weight of the load cell, which is connected to the microcontroller
         Return: The weight in grams"""
-        # TODO: needs to be implemented
-        pass
+        self.bus.write_i2c_block_data(self.address, 0, "start_meas")  # int
+        raw_data = self.bus.read_i2c_block_data(self.address, 0, 16)
+        return json.loads(raw_data)['weight']
 
     @staticmethod
     def get_instance():
