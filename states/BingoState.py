ac6865c438f9c0eba847fb921ba3ea13e7e88b86
qlimax89 <Kevin_25jan@hotmail.com>
2021-06-08 11:09:10 +0200

    bingo state begin

--- states/BingoState.py
+++ states/BingoState.py
@@ -1,12 +1,19 @@
 from states.State import State
+from datetime import datetime
+import random
 
 
 class BingoState(State):
     def __init__(self):
-        pass
+        self.startTime = datetime.now()
+        self.bingoNumber = random.randint(1, 75)
 
     def step(self):
-        pass
+        randomList = []
+        for randomNumbers in range(1, 75):
+            self.bingoNumber = random.randint(1, 75)
+            randomList.append(self.bingoNumber)
+            print(randomList)
 
     def deactivate(self):
         pass
