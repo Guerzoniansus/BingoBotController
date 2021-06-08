c020b0b4c483eae2f284e8fcbe89861168abe871
Stewie02 <stefansijbesma12@gmail.com>
2021-06-08 11:34:10 +0200

    Merge branch 'Boundary-classes' into main

--- parts/driving/Motor.py
+++ parts/driving/Motor.py
@@ -4,12 +4,25 @@ from abc import ABC, abstractmethod
 class Motor(ABC):
 
     def __init__(self, max_speed):
+        """Sets the max speed
+        max_speed: Double which represents the maximum motor speed of the motor"""
         self.MAX_SPEED = max_speed
-        super().__init__()
+        self.current_speed = 0
 
     @abstractmethod
     def set_speed(self, speed):
+        """Sets the speed in of the motor
+        speed: Double representing the speed"""
         pass
+
+    def _set_current_speed(self, speed):
+        """Sets the current_speed variable
+        speed: The new double value for the current_speed"""
+        self.current_speed = speed
+
+    def get_speed(self):
+        """Returns the current speed"""
+        return self.current_speed
 
     def _limit_speed(self, speed):
         """ A function that makes sure speed is not above max speed or below minimum speed.
--- parts/remote/RemoteControl.py
+++ parts/remote/RemoteControl.py
@@ -4,58 +4,110 @@ from parts.remote.ControllerButton import ControllerButton
 import socket
 import json
 
-listeners = []
-__buffer_size = 1024
-__local_ip = '141.252.29.9'
-__local_port = '9010'
-__udpServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-__running = False
+class RemoteControl:
 
+    __listeners = []
+    __thread = None
+    __buffer_size = 1024
+    __local_ip = '141.252.29.9'
+    __local_port = '9010'
+    __udpServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    __running = False
+    __instance = None
 
-def _on_message_received(data_object):
-    if data_object['mode'] == 'manual':
-        __received_manual_data(data_object)
+    def __init__(self):
+        """Creates a instance of the RemoteController if none exists
+        Else an exception is raised"""
+        if RemoteControl.__instance is not None:
+            raise Exception('This class is a singleton!')
 
-def __received_manual_data(data_object):
-    for listener in listeners:
-        listener.on_joystick_change(data_object['left_joy_Y'], data_object['right_joy_Y'])
+        self.__udpServerSocket.bind((self.__local_ip, self.__local_port))
+        self.__running = True
 
-        if data_object['gripper'] == 'open':
-            listener.on_button_press(ControllerButton.GRIPPER_OPEN)
-        elif data_object['gripper'] == 'close':
-            listener.on_button_press(ControllerButton.GRIPPER_CLOSE)
+    def __on_message_received(self, data_object):
+        """Is called when new data is received from the remote controller
+        data_object: The parse JSON data"""
+        # If the mode is manual the manual data will be send
+        if data_object['mode'] == 'manual':
+            self.__received_manual_data(data_object)
+        # Always will be sent in which mode the robot is in
+        self.__send_mode(data_object)
 
-        if data_object['arm'] == 'up':
-            listener.on_button_press(ControllerButton.ARM_UP)
-        elif data_object['arm'] == 'down':
-            listener.on_button_press(ControllerButton.ARM_DOWN)
+    def __received_manual_data(self, data_object):
+        """Is called when data of the manual mode is received
+        All listeners will be informed with the information
+        data_object: All the data from the request"""
+        for listener in self.__listeners:
+            listener.on_joystick_change(data_object['left_joy_Y'], data_object['right_joy_Y'])
 
-def add_listener(listener):
-    """Add a RemoteControl listener to this remote controller that gets notified for controller events.
-    A RemoteControl listener should have the following two public functions:
-    - on_button_press(ControllerButton)
-    - on_joystick_change(left_amount, right_amount)
-    """
-    listeners.append(listener)
+            if data_object['gripper'] == 'open':
+                listener.on_button_press(ControllerButton.GRIPPER_OPEN)
+            elif data_object['gripper'] == 'close':
+                listener.on_button_press(ControllerButton.GRIPPER_CLOSE)
 
-def remove_listener(listener):
-    """Remove a RemoteControl listener from the list of listeners."""
-    listeners.remove(listener)
+            if data_object['arm'] == 'up':
+                listener.on_button_press(ControllerButton.ARM_UP)
+            elif data_object['arm'] == 'down':
+                listener.on_button_press(ControllerButton.ARM_DOWN)
 
-def __run():
-    while __running:
-        bytes_address_pair = __udpServerSocket.recvfrom(__buffer_size)
-        message = bytes_address_pair[0].decode("utf-8")
-        _on_message_received(json.loads(message))
+    def __send_mode(self, data_object):
+        """Sends the mode to all the different listeners"""
+        button = None
+        if data_object['mode'] == 'bingo':
+            button = ControllerButton.BINGO
+        elif data_object['mode'] == 'manual':
+            button = ControllerButton.MANUAl
+        elif data_object['mode'] == 'dance_preprogrammed':
+            button = ControllerButton.DANCE_PREPROGRAMMED
+        elif data_object['mode'] == 'dance_autonome':
+            button = ControllerButton.DANCE_AUTONOME
+        elif data_object['mode'] == 'autonome_route':
+            button = ControllerButton.AUTONOME_ROUTE
 
+        for listener in self.__listeners:
+            if button is not None:
+                listener.on_button_press(button)
 
-__thread = threading.Thread(target=__run)
+    def add_listener(self, listener):
+        """Add a RemoteControl listener to this remote controller that gets notified for controller events.
+        A RemoteControl listener should have the following two public functions:
+        - on_button_press(ControllerButton)
+        - on_joystick_change(left_amount, right_amount)
+        """
+        self.__listeners.append(listener)
 
+    def remove_listener(self, listener):
+        """Remove a RemoteControl listener from the list of listeners."""
+        self.__listeners.remove(listener)
 
-def start():
-    __udpServerSocket.bind((__local_ip, __local_port))
-    __file__.__running = True
+    def __run(self):
+        """Is run when listening to the remote controller"""
+        while self.__running:
+            bytes_address_pair = self.__udpServerSocket.recvfrom(self.__buffer_size)
+            message = bytes_address_pair[0].decode("utf-8")
+            self.__on_message_received(json.loads(message))
 
-def stop():
-    __file__.__running = False
-    __thread.join()
+    def start(self):
+        """Starts the RemoteController"""
+        self.__running = True
+        self.__thread = threading.Thread(target=self.__run)
+
+    def stop(self):
+        """Stops the RemoteController"""
+        self.__running = False
+        if self.__thread is not None:
+            if self.__thread.is_alive():
+                self.__thread.join()
+        self.__thread = None
+
+    def __del__(self):
+        self.stop()
+
+    @staticmethod
+    def get_instance():
+        """Returns a instance if the RemoteController
+        The existing one or a new one will be created
+        Return: the new or existing instance"""
+        if RemoteControl.__instance is None:
+            RemoteControl.__instance = RemoteControl()
+        return RemoteControl.__instance
