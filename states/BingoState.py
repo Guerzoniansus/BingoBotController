134fd902fc758bb9f5c7d146707160e12fe593d6
qlimax89 <Kevin_25jan@hotmail.com>
2021-06-11 14:50:24 +0200

    added bingolist need code from erik and stefan

--- states/BingoState.py
+++ states/BingoState.py
@@ -1,21 +1,28 @@
 from states.State import State
-from datetime import datetime
 from parts.driving import DrivingHandler
 import random
+import time
 
 
 class BingoState(State):
     def __init__(self):
-        self.startTime = datetime.now()
-        self.bingoNumber = random.randint(1, 75)
+        self.bingoNumberList = [*range(1, 76, 1)]
 
     def step(self):
         """" Make a list with numbers that are random added """
-        randomList = []
-        for randomNumbers in range(1, 75):
-            self.bingoNumber = random.randint(1, 75)
-            randomList.append(self.bingoNumber)
-            print(randomList)
+        while len(self.bingoNumberList) > 1:
+            targetDigit = random.randint(0, len(self.bingoNumberList) - 1)
+            # print("bingo list na poppen", bingoNumberList)
+            print("remove: ", self.bingoNumberList[targetDigit])
+            self.bingoNumberList.pop(targetDigit)
+            time.sleep(3)
+            # if onheard is True:
+            # erik zijn code
+            # if bingoCardNumbers is in bingoNumberList:
+            # bingo = true #stefan zijn code
+            # else:
+            # Binog = false
+
 
     def deactivate(self):
         """Function that should be run when switching away from this state"""
