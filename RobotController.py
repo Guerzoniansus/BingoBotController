d967465738df8ae84cb7fb7d6d01e23a43b8fbd9
erikvanderhorst <erikvanderhorst@hotmail.com>
2021-06-14 15:55:44 +0200

    nieuwe tekst

--- RobotController.py
+++ RobotController.py
@@ -42,7 +42,7 @@ class RobotController(RemoteControlListener):
         webConnection.start()
 
         output = AudioOutputHandler.get_instance()
-        output.speak("Ik ben de bingo bot en ", "test")
+        output.speak("1,2,3,4,5,6,7,8,9", "test")
 
     @staticmethod
     def get_instance():
