123ddfb4edc3a523762dd144ee83cb001c134167
erikvanderhorst <erikvanderhorst@hotmail.com>
2021-06-21 12:47:04 +0200

    changed ip to pi ip

--- web_connection/WebConnection.py
+++ web_connection/WebConnection.py
@@ -40,7 +40,7 @@ class WebConnection:
         """
             start a websocket server on a new thread
         """
-        server_host = "192.168.137.49"
+        server_host = "192.168.137.242`"
         server_port = 9010  # random.randint(10000, 60000)
         new_loop = asyncio.new_event_loop()
         start_server = websockets.serve(self.__send_data, server_host, server_port, loop=new_loop)
