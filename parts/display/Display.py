0a93536d68f9716b5fb1cec90334cbdcd26a41fa
Stewie02 <stefansijbesma12@gmail.com>
2021-06-22 12:01:00 +0200

    Added the show_vu method

--- parts/display/Display.py
+++ parts/display/Display.py
@@ -4,3 +4,8 @@ from parts.MicroController import MicroController
 def show_bingo_number_on_display(number):
     """Shows the number on the display connected to the MicroController"""
     MicroController.get_instance().show_bingo_number_on_display(number)
+
+
+def show_vu(low, mid, high):
+    """Sends the data to the MicroController"""
+    pass
