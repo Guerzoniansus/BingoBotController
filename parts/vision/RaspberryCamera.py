300e2debaae40336535b3ab5be38b9ff6624e419
erikvanderhorst <erikvanderhorst@hotmail.com>
2021-06-22 12:40:45 +0200

    added pil

--- parts/vision/RaspberryCamera.py
+++ parts/vision/RaspberryCamera.py
@@ -45,7 +45,6 @@ if Constants.USING_PI_CAMERA:
             Img.save(Output_buffer, format='JPEG')
             Byte_data = Output_buffer.getvalue()
             Base64_data = base64.b64encode(Byte_data)
-            print(Base64_data)
             return self.__parse(Base64_data)
 
         def __parse(self, base64_encoded_string):
