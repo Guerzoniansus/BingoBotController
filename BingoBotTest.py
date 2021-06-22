605a811cc8903984de84c6ca53838e077526abad
Stewie02 <stefansijbesma12@gmail.com>
2021-06-22 15:00:54 +0100

    Changed the test

--- BingoBotTest.py
+++ BingoBotTest.py
@@ -1,5 +1,6 @@
 # Just import the test you want to run!
 #import tests.parts.remote.RemoteTester
 
-import tests.parts.sensors.DistanceTest
+# import tests.parts.sensors.DistanceTest
 
+import tests.parts.display.DisplayTest
