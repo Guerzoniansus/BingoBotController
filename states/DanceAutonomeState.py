f4d9cf825fccac90d2333fd7fa648dda75ec95e8
ElvedinIbric <55244146+ElvedinIbric@users.noreply.github.com>
2021-06-23 10:53:09 +0200

    commit commit

--- states/DanceAutonomeState.py
+++ states/DanceAutonomeState.py
@@ -1,54 +1,109 @@
+import struct
+
+import numpy as np
+
 from parts.driving import DrivingHandler
 from parts.remote import RemoteControl
 from parts.remote.ControllerButton import ControllerButton
 from states.State import State
-from states.moves.ArmMove import ArmMove
-from states.moves.DriveMove import DriveMove
-from states.moves.GripMove import GripMove
-#   import RPi.GPIO as GPIO
+import pyaudio
+
+# Audio setup
+CHUNK = 1024 * 4  # number of data points to read at a time
+RATE = 44100  # time resolution of the recording device (Hz)
+CHANNELS = 1
+DEVICE = 1  # device number
+LOWBAR = 100
+MIDDLEBAR = 200
 
 
 class DanceAutonomeState(State):
+
     def __init__(self):
         RemoteControl.add_listener(self)
-        # GPIO.setwarnings(False)  # Ignore warning for now
-        # GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-        # GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
-        # GPIO.setup(9, GPIO.OUT, initial=GPIO.LOW)
-        # GPIO.setup(10, GPIO.OUT, initial=GPIO.LOW)
 
-        self.route_index = 0
-        self.song_time = 120
+        p = pyaudio.PyAudio()  # start the PyAudio class
+        stream = p.open(format=pyaudio.paInt16,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        input=True,
+                        output=True,
+                        frames_per_buffer=CHUNK
+                        )  # uses default input device
 
-        # Which speeds to use for which motors for which directions
-        # The first double is the left motor speed, the second double is the right motor speed
-        self._SPEEDS = [
-            [7.0, 7.0],  # Forward
-            [-7.0, -7.0],  # Backward
-            [-4.0, 4.0],  # Left
-            [4.0, -4.0]  # Right
-        ]
-
-        pass
+    np.set_printoptions(suppress=True)  # don't use scientific notation
 
     def step(self):
-        # TODO: code for VU sound action
-        # TODO: VU_Meter class
-        # if VU_value == HIGH:
-        #     GPIO.output(8, GPIO.HIGH)  # Turn on
-        #     DriveMove(self._SPEEDS[2]) # Dance to the left
-        #     Logger.get_instance().log("Spin to the left")
-        #     GPIO.output(8, GPIO.LOW)   # Turn off
-        # elif VU_value == MIDDLE:
-        #     GPIO.output(9, GPIO.HIGH)  # Turn on
-        #     DriveMove(self._SPEEDS[3]) # Dance to the right
-        #     Logger.get_instance().log("Spin to the right")
-        #     GPIO.output(9, GPIO.LOW)   # Turn off
-        # elif VU_value == LOW:
-        #     GPIO.output(10, GPIO.HIGH)  # Turn on
-        #     ArmMove(self.time_for_move)
-        #     Logger.get_instance().log("Move the arm up and down")
-        #     GPIO.output(10, GPIO.LOW)   # Turn off
+        data = struct.unpack(str(CHUNK) + 'h', stream.read(CHUNK))
+        data = np.array(data)
+        data_fft = np.fft.fft(data)
+        frequencies = np.fft.fftfreq(len(data_fft))
+        highest = np.argpartition(np.abs(data_fft), range(10))[:10]
+        print(highest)
+
+        low, mid, high = 0, 0, 0
+
+        for index in highest:
+            loudness = np.abs(data_fft[index])
+            print("loudness: ", loudness)
+            freq = frequencies[index]
+            freq = abs(freq * RATE)
+            print(freq)
+
+            if freq <= 8000:  # low frequency sound
+                if loudness > LOWBAR:
+                    low = + 1
+                    print("low lower")
+                elif LOWBAR <= loudness <= MIDDLEBAR:
+                    low = + 2
+                    print("low middle")
+                elif loudness > MIDDLEBAR:
+                    low = + 3
+                    print("low higher")
+
+            elif freq >= 20000:  # high frequency sound
+                if loudness > LOWBAR:
+                    high = + 1
+                    print("high lower")
+                elif LOWBAR <= loudness <= MIDDLEBAR:
+                    high = + 2
+                    print("high middle")
+                elif loudness > MIDDLEBAR:
+                    high = + 3
+                    print("high higher")
+
+            else:  # otherwise it's middle frequency sound
+                if loudness > LOWBAR:
+                    mid = + 1
+                    print("mid lower")
+                elif LOWBAR <= loudness <= MIDDLEBAR:
+                    mid = + 2
+                    print("mid middle")
+                elif loudness > MIDDLEBAR:
+                    mid = + 3
+                    print("mid higher")
+
+        if low != 0:  # when there is low frequencies found light the led amount
+            if low == 1:
+                print("Low: #")
+            elif low == 2:
+                print("Low: ##")
+            else:
+                print("Low: ###")
+        if mid != 0:  # when there is middle frequencies found light the led amount
+            if mid == 1:
+                print("Middle: #")
+            elif mid == 2:
+                print("Middle: ##")
+            else:
+                print("Middle: ###")
+        if high != 0:  # when there is high frequencies found light the led amount
+            if high == 1:
+                print("High: #")
+            elif high == 2:
+                print("High: ##")
+            else:
+                print("High: ###")
 
         pass
 
